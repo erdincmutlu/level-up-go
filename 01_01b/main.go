fe0c0ad4fe034ab170c8f5ad44c8626a0b7f3fa7
Erdinc Mutlu <erdincmutlu@gmail.com>
2025-10-17 09:46:46 +0100

    Sleep until your birthday

---
+++ 01_01b/main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"flag"
+	"log"
+	"time"
+)
+
+var expectedFormat = "2006-01-02"
+
+// parseTime validates and parses a given date string.
+func parseTime(target string) time.Time {
+	parsed, err := time.Parse(expectedFormat, target)
+	if err != nil || time.Now().After(parsed) {
+		log.Fatalf("invalid date, %s", target)
+	}
+
+	return parsed
+}
+
+// calcSleeps returns the number of sleeps until the target.
+func calcSleeps(target time.Time) float64 {
+	diff := time.Until(target)
+	return diff.Hours() / 24
+}
+
+func main() {
+	bday := flag.String("bday", "", "Your next bday in YYYY-MM-DD format")
+	flag.Parse()
+	target := parseTime(*bday)
+	log.Printf("You have %d sleeps until your birthday. Hurray!",
+		int(calcSleeps(target)))
+}
